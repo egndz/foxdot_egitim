36e0ccf26392923c1b9c846333cc75b7c45d7e57
egndz <egehangunduz@gmail.com>
2020-01-22 14:14:21 +0300

    .DS_Store banished!

--- egehan/downloaders/macos-downloader.sh
+++ egehan/downloaders/macos-downloader.sh
@@ -1,6 +1,5 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && 
 python get-pip.py && 
-pip install FoxDot &&
 git clone https://github.com/Qirky/FoxDot.git &&
 cd FoxDot && 
 python setup.py install 
